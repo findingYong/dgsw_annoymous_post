bc4a9edf4673de9250d02663c69692295d604836
yongin1212 <godghr10031@gmail.com>
2026-06-24 14:03:29 +0900

    first commit

---
+++ src/main/java/org/dgsw/finding_yong/FindingYongApplication.java
@@ -0,0 +1,13 @@
+package org.dgsw.finding_yong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindingYongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindingYongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/dgsw/finding_yong/FindingYongApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dgsw.finding_yong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindingYongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
